74d6205e91df5f6f4724f0fe121c95c58dd7fca2
Erick <egavin1992@gmail.com>
2020-10-23 21:03:36 -0400

    add refs for GSAP

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import setSingleProject from '../Contentful/SetSingleProject'
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+import CssRulePlugin from 'gsap/CSSRulePlugin'
+import ImageReveal from 'ProjectGSAP'
+import gsap from 'gsap'
 
 export default function SingleProject() {
-    var parse = require('html-react-parser');
+    let parse = require('html-react-parser')
     const { id } = useParams()
     const [project, isLoading] = setSingleProject(id)
+    let container = useRef(null)
+    let image = useRef(null)
+    let imageReveal = CssRulePlugin.getRule('.project__intro__img:after')
+
+    let tl = gsap.timeline({defaults: {duration: 1}})
+
+    useEffect(() => {
+        // tl.to(container, {duration: 1, css: {visibility: "visible"}}
+        //     .to(imageReveal, {duration: 1.4, width: "0%"})
+    })
     
     
     const renderProject = () => {
@@ -15,10 +28,13 @@ export default function SingleProject() {
       return (
         <>
             <div className="project__intro" style={{backgroundColor: project.color}}>
-                <img
-                    className="project__intro__img"
-                    src={project.secondaryImage.fields.file.url}
-                />
+                <div className="project__intro__img" ref={el => container = el}>
+                    <img
+                        ref={el => image = el}
+                        className="project__intro__img__main"
+                        src={project.secondaryImage.fields.file.url}
+                    />
+                </div>
                 <div className="project__intro__text">
                     <h2 id="left" className="project__intro__text__title" >{project.title}</h2>
                     <h2 id="right" className="project__intro__text__title">{project.title}</h2>
---
+++ src/components/Projects/ProjectGSAP.js
@@ -0,0 +1,9 @@
+import gsap from 'gsap'
+
+export default function ImageReveal(container, image) {
+
+    let tl = gsap.timeline({defaults: {duration: 1, delay: 2}})
+
+    tl.from()
+
+}
